b7eedc1a1a92e325967a18478b9bb829888793f1
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2020-11-24 21:35:31 +0000

    Request-Certificate tidy up

--- Request-Certificate.ps1
+++ Request-Certificate.ps1
@@ -33,7 +33,7 @@ Kudos to jbpaux for contributing improvements and fixes on GitHub!
 #>
 
 <#
-.SYNOPSIS 
+.SYNOPSIS
 Requests a certificate from a Windows CA
 
 .DESCRIPTION
@@ -46,9 +46,9 @@ The CA must support this type of certificate otherwise the request will fail.
 
 With the Export parameter it's also possible to export the requested certificate (with private key) directly to a .pfx file instead of storing it in the local computer store.
 
-You can also use the Import-CSV cmdlet with Request-Certificate.ps1 to request multiple certificates. 
+You can also use the Import-CSV cmdlet with Request-Certificate.ps1 to request multiple certificates.
 To do this, use the Import-CSV cmdlet to create custom objects from a comma-separated value (CSV) file that contains a list of object properties (such as CN, SAN etc. ). Then pass these objects through the pipeline to Request-Certificate.ps1 to request the certificates.
-    
+
 .PARAMETER CN
 Specifies the common name for the subject of the certificate(s).
 Mostly its the FQDN of a website or service.
@@ -57,15 +57,15 @@ e.g. test.jofe.ch
 .PARAMETER SAN
 Specifies a comma separated list of subject alternate names (FQDNs) for the certificate
 The syntax is {tag}={value}.
-Valid tags are: email, upn, dns, guid, url, ipaddress, oid 
+Valid tags are: email, upn, dns, guid, url, ipaddress, oid
 e.g. dns=test.jofe.ch,email=jfeller@jofe.ch
 
 .PARAMETER TemplateName
-Specifies the name for the temple of the CA to issue the certificate(s). 
+Specifies the name for the temple of the CA to issue the certificate(s).
 The default value is "WebServer".
 
 .PARAMETER KeyLength
-Specifies the key length in Bit for the certificate. 
+Specifies the key length in Bit for the certificate.
 Possible Values: 1024,2048,3072,4096,15360
 Default Value: 2048
 
@@ -110,7 +110,7 @@ Specify the Password (as plain String or SecureString) used on the export.
 
 .INPUTS
 System.String
-Common name for the subject, SAN , Country, State etc. of the certificate(s) as a string 
+Common name for the subject, SAN , Country, State etc. of the certificate(s) as a string
 
 .OUTPUTS
 None. Request-Certificate.ps1 does not generate any output.
@@ -133,25 +133,25 @@ The user will be asked for the value for the CN of the certificate.
 
 .EXAMPLE
 C:\PS> .\Request-Certificate.ps1 -CN "webserver.test.ch" -CAName "testsrv.test.ch\Test CA" -TemplateName "Webservercert"
- 
+
 Description
 -----------
 This command requests a certificate form the CA testsrv.test.ch\Test CA with the certificate template "Webservercert"
 and a CN of webserver.test.ch
-The user will be asked for the value for the SAN of the certificate. 
+The user will be asked for the value for the SAN of the certificate.
 
- 
+
 .EXAMPLE
 Get-Content .\certs.txt | .\Request-Certificate.ps1 -Export
 
 Description
 -----------
-Gets common names from the file certs.txt and request for each a certificate. 
+Gets common names from the file certs.txt and request for each a certificate.
 Each certificate will then be saved withe the private key in a .pfx file.
 
 .EXAMPLE
 C:\PS> .\Request-Certificate.ps1 -CN "webserver.test.ch" -SAN "DNS=webserver.test.ch,DNS=srvweb.test.local"
- 
+
 Description
 -----------
 This command requests a certificate with a CN of webserver.test.ch and subject alternative names (SANs)
@@ -159,7 +159,7 @@ The SANs of the certificate are the DNS names webserver.test.ch and srvweb.test.
 
 .EXAMPLE
 C:\PS> Import-Csv .\sancertificates.csv -UseCulture | .\Request-Certificate.ps1 -verbose -Export -CAName "testsrv.test.ch\Test CA"
- 
+
 Description
 -----------
 This example requests multiple SAN certificates from the "Test CA" CA running on the server "testsrv.test.ch".
@@ -171,15 +171,15 @@ CN;SAN
 test1.test.ch;DNS=test1san1.test.ch,DNS=test1san2.test.ch
 test2.test.ch;DNS=test2san1.test.ch,DNS=test2san2.test.ch
 test3.test.ch;DNS=test3san1.test.ch,DNS=test3san2.test.ch
-		   
+
 .NOTES
 
 Version    : 1.4, 01/31/2019
-Changes    : 
+Changes    :
 	Thanks to David Allsopp c/o dra27 on GitHub
-	- Better default for CAName so it is not needed when only one CA is available. 
+	- Better default for CAName so it is not needed when only one CA is available.
 	- ProviderName specified in the request inf file
-	
+
 Version    : 1.3, 10/20/2018
 Changes    :
 	- Improvements in temp file handling
@@ -198,40 +198,38 @@ www.jfe.cloud
 [CmdletBinding(DefaultParametersetname="NoExport")]
 Param(
     [Parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
-    [string]$CN,
+    [String]$CN,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string[]]$SAN,
+    [String[]]$SAN,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
     [String]$TemplateName = "WebServer",
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
     [ValidateSet(1024,2048,3072,4096,15360)]
     [int]$keyLength = 2048,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string]$CAName,
+    [String]$CAName,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string]$Country,
+    [String]$Country,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string]$State,
+    [String]$State,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string]$City,
+    [String]$City,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string]$Organisation,
+    [String]$Organisation,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [string]$Department,
+    [String]$Department,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True)]
-    [switch]$AddCNinSAN,
+    [Switch]$AddCNinSAN,
     [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $True, ParameterSetName='Export')]
-    [switch]$Export,
+    [Switch]$Export,
     [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $True, ParameterSetName='Export')]
     [ValidateScript( {Resolve-Path -Path $_})]
-    [string]$ExportPath,
+    [String]$ExportPath,
     [Parameter(Mandatory = $False, ValueFromPipelineByPropertyName = $True, ParameterSetName='Export')]
-    [ValidateScript( {$_.getType().name -eq "SecureString" -or $_.getType().name -eq "String"})]
-    $Password
-
+    [System.Security.SecureString]$Password
 )
+
 BEGIN {
-    #internal function to do some cleanup
     function Remove-ReqTempfiles() {
         param(
             [String[]]$tempfiles
@@ -246,26 +244,37 @@ BEGIN {
         )
         Write-Verbose "Remove pending certificate request form cert store..."
 
-        #delete pending request (if a request exists for the CN)
-        $certstore = new-object system.security.cryptography.x509certificates.x509Store('REQUEST', 'LocalMachine')
+        # Delete pending request (if a request exists for the CN)
+        $certstore = New-Object System.Security.Cryptography.X509Certificates.X509Store('REQUEST', 'LocalMachine')
         $certstore.Open('ReadWrite')
         foreach ($certreq in $($certstore.Certificates)) {
             if ($certreq.Subject -eq "CN=$CN") {
+                Write-Output "Removing existing pending request for CN: $CN"
                 $certstore.Remove($certreq)
             }
         }
         $certstore.close()
     }
 }
-
 PROCESS {
-    #disable debug confirmation messages
+    # Disable debug confirmation messages
     if ($PSBoundParameters['Debug']) {$DebugPreference = "Continue"}
 
     Write-Verbose "Generating request inf file"
+
+    $subjectVars = [ordered]@{ 'CN' = 'cn'; 'Country' = 'c'; 'State' ='s'; 'City' = 'l'; 'Organisation' = 'o'; 'Department' = 'ou'; }
+    $subjectArray = New-Object System.Collections.ArrayList
+
+    foreach ($var in $subjectVars.GetEnumerator()) {
+        if (((Get-Variable -Name $var.Name -ValueOnly) -ne '') -and ($null -ne (Get-Variable -Name $var.Name -ValueOnly))) {
+            $paramValue = Get-Variable -Name $var.Name -ValueOnly
+            $subjectArray.Add("$($var.Value)=$paramValue") | Out-Null
+        }
+    }
+
     $file = @"
 [NewRequest]
-Subject = "CN=$CN,c=$Country, s=$State, l=$City, o=$Organisation, ou=$Department"
+Subject = "$($subjectArray -join ',')"
 MachineKeySet = TRUE
 KeyLength = $KeyLength
 KeySpec=1
@@ -276,32 +285,30 @@ ProviderName = "Microsoft Enhanced Cryptographic Provider v1.0"
 CertificateTemplate = "$TemplateName"
 "@
 
-    
-    #check if SAN certificate is requested
+    # Check if SAN certificate is requested
     if ($PSBoundParameters.ContainsKey('SAN')) {
-        #each SAN must be a array element
-        #if the array has ony one element then split it on the commas.
+        # Each SAN must be a array element
+        # If the array has ony one element then split it on the commas.
         if (($SAN).count -eq 1) {
             $SAN = @($SAN -split ',')
         }
     }
 
     if ($AddCNinSAN) {
-        $SAN = "DNS=$CN" + $SAN #Add CN as first SAN entry
+        $SAN += "DNS=$CN" # Add CN as first SAN entry
     }
 
     # Remove Potential duplicates (if CN was already provided in SAN list)
     $SAN = $SAN | Select-Object -Unique
-
 
     if ($SAN.Count -gt 0) {
 
         Write-Host "Requesting SAN certificate with subject $CN and SAN: $($SAN -join ',')" -ForegroundColor Green
         Write-Debug "Parameter values: CN = $CN, TemplateName = $TemplateName, CAName = $CAName, SAN = $($SAN -join ' ')"
 
-        Write-Verbose "A value for the SAN is specified. Requesting a SAN certificate." 
+        Write-Verbose "A value for the SAN is specified. Requesting a SAN certificate."
         Write-Debug "Add Extension for SAN to the inf file..."
-        $file += 
+        $file +=
 @'
 
 [Extensions]
@@ -323,8 +330,11 @@ CertificateTemplate = "$TemplateName"
 
     Write-Debug "Inf-File: $file"
 
+    Write-Output $file
+    exit
+
     try	{
-        #create temp files
+        # Create temp files
         $inf = [System.IO.Path]::GetTempFileName()
         $req = [System.IO.Path]::GetTempFileName()
         $filename = $CN -replace "^\*","wildcard"
@@ -333,23 +343,23 @@ CertificateTemplate = "$TemplateName"
 
         Remove-ReqTempfiles -tempfiles $inf, $req, $cer, $rsp
 
-        #write the file to debug logs (if debug enabled)
+        # Write the file to debug logs (if debug enabled)
         Write-Debug $file
-        #create new request inf file
-        Set-Content -Path $inf -Value $file 
+        # Create new request inf file
+        Set-Content -Path $inf -Value $file
 
-        #show inf file if -verbose is used
+        # Show inf file if -verbose is used
         Get-Content -Path $inf | Write-Verbose
 
-        Write-Verbose "generate .req file with certreq.exe"
+        Write-Verbose "Generate .req file with certreq.exe"
         Invoke-Expression -Command "certreq -new `"$inf`" `"$req`""
         if (!($LastExitCode -eq 0)) {
             throw "certreq -new command failed"
         }
 
-        write-verbose "Sending certificate request to CA"
+        Write-Verbose "Sending certificate request to CA"
         Write-Debug "CAName = $CAName"
-            
+
         if (!$PSBoundParameters.ContainsKey('CAName')) {
             $rootDSE = [System.DirectoryServices.DirectoryEntry]'LDAP://RootDSE'
             $searchBase = [System.DirectoryServices.DirectoryEntry]"LDAP://$($rootDSE.configurationNamingContext)"
@@ -384,7 +394,7 @@ CertificateTemplate = "$TemplateName"
 
         if (($LastExitCode -eq 0) -and ($? -eq $true)) {
             Write-Host "Certificate request successfully finished!" -ForegroundColor Green
-		    	
+
         }
         else {
             throw "Request failed with unknown error. Try with -verbose -debug parameter"
@@ -397,7 +407,7 @@ CertificateTemplate = "$TemplateName"
             $cert = Get-Childitem "cert:\LocalMachine\My" | where-object {$_.Thumbprint -eq (New-Object System.Security.Cryptography.X509Certificates.X509Certificate2((Get-Item $cer).FullName, "")).Thumbprint}
             Write-Debug "Certificate found in computer store: $cert"
 
-            #create a pfx export as a byte array
+            # Create a pfx export as a byte array
             if($Password) {
                 Write-Debug "Exporting with password"
                 $certbytes = $cert.export([System.Security.Cryptography.X509Certificates.X509ContentType]::pfx, $Password)
@@ -405,25 +415,25 @@ CertificateTemplate = "$TemplateName"
                 Write-Debug "Exporting without password"
                 $certbytes = $cert.export([System.Security.Cryptography.X509Certificates.X509ContentType]::pfx)
             }
-            
 
-            #write pfx file
+
+            # Write pfx file
             if ($PSBoundParameters.ContainsKey('ExportPath')) {
-                $pfxPath = Join-Path -Path (Resolve-Path -Path $ExportPath) -ChildPath "$filename.pfx" 
+                $pfxPath = Join-Path -Path (Resolve-Path -Path $ExportPath) -ChildPath "$filename.pfx"
             }
             else {
                 $pfxPath = ".\$filename.pfx"
             }
             $certbytes | Set-Content -Encoding Byte -Path $pfxPath -ea Stop
             Write-Host "Certificate successfully exported to `"$pfxPath`"!" -ForegroundColor Green
-		    
+
             Write-Verbose "deleting exported certificate from computer store"
-            # delete certificate from computer store
+            # Delete certificate from computer store
             $certstore = new-object system.security.cryptography.x509certificates.x509Store('My', 'LocalMachine')
             $certstore.Open('ReadWrite')
             $certstore.Remove($cert)
-            $certstore.close() 
-		    
+            $certstore.close()
+
         }
         else {
             Write-Debug "export parameter is not set. => script finished"
@@ -431,11 +441,11 @@ CertificateTemplate = "$TemplateName"
         }
     }
     catch {
-        #show error message (non terminating error so that the rest of the pipeline input get processed) 
+        # Show error message (non terminating error so that the rest of the pipeline input get processed)
         Write-Error $_
     }
     finally {
-        #tempfiles and request cleanup
+        # Tempfiles and request cleanup
         Remove-ReqTempfiles -tempfiles $inf, $req, $cer, $rsp
         Remove-ReqFromStore -CN $CN
     }
